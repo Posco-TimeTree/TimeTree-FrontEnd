2b039669416b4e7db348c411b49b495562cd749b
nanajin <nmj5182@gmail.com>
2023-07-19 14:06:10 +0900

    feat: axios 설정

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common["Referrer-Policy"] = "no-referrer-when-downgrade";
+
+const axiosConfig = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+  },
+  timeout: 30000
+});
+
+export default axiosConfig;
