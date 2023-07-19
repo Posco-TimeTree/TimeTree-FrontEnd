0a095a82fafb0cd827eb12198459fcd0af23f0e8
nanajin <nmj5182@gmail.com>
2023-07-19 13:38:29 +0900

    feat: mainpage 진행중

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Canvas from '../components/Canvas';
 import SideBar from '../components/SideBar';
+import styled from 'styled-components';
 
 const MainPage = () => {
   return (
     <div>
-      <Canvas/>
+      <LinkButton>
+        <Link to={"/make-my-tree"}>
+          나만의 트리 꾸미러 가기 ▶
+        </Link>
+      </LinkButton>
     </div>
   );
 };
-
+const LinkButton = styled.div`
+  font-size: 30px;
+  width: fit-content;
+  height: fit-content;
+  padding: 10px;
+  border-radius: 2em;
+  color: black;
+  // background-color: ;
+`
 export default MainPage;
