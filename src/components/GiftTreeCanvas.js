c64965e690afbfc5969d7c026bc8ec52b8112831
nanajin <nmj5182@gmail.com>
2023-07-20 10:04:24 +0900

    feat: canvas 크기 수정

--- src/components/GiftTreeCanvas.js
+++ src/components/GiftTreeCanvas.js
@@ -152,7 +152,7 @@ export default function GiftTreeCanvas() {
 }
 
 const Wrapper = styled.div`
-  height: 100vh;
+  // height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: row;
@@ -173,7 +173,8 @@ const CanvasContainer = styled.div`
   background-image: url(${(props) => props.backgroundImg});
   background-position-x: center;
   background-position-y: -130px;
-  background-size: 853px 1280px;
+  // background-size: 853px 1280px;
+  background-size: 700px 1040px;
   background-repeat: no-repeat;
   background-color: #8aacbf87;
 `;
