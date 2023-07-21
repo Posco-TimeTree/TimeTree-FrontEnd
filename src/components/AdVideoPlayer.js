9ef3da6c1b009a7f40a771bf20e78fe2a2aae9db
이유진 <rooproop1111@naver.com>
2023-07-21 11:48:33 +0900

    add AD
    - ad 적용

--- src/components/AdVideoPlayer.js
+++ src/components/AdVideoPlayer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import styled from 'styled-components';
 
-const VideoPlayer = (props) => {
+const VideoPlayer = () => {
   const [showCancelButton, setShowCancelButton] = useState(false);
 
   useEffect(() => {
@@ -15,36 +15,17 @@ const VideoPlayer = (props) => {
     };
   }, []);
 
-  const handleCancelButtonClick = () => {
-    setShowCancelButton(false);
-    props.onVideoEnd();
-  };
-
   return (
     <div>
       <ReactPlayer
         url="/ad.mp4" // 여기에 MP4 파일의 URL을 입력합니다.
         playing // 자동으로 재생
         controls // 재생 컨트롤러 표시
-        width="640px" // 동영상 플레이어의 너비
-        height="360px" // 동영상 플레이어의 높이
+        width="1280px" // 동영상 플레이어의 너비
+        height="720px" // 동영상 플레이어의 높이
       />
-      {showCancelButton && (
-        <CancleButton onClick={handleCancelButtonClick}>Skip Ad</CancleButton>
-      )}
     </div>
   );
 };
 
 export default VideoPlayer;
-
-const CancleButton = styled.button`
-  width: 220px;
-  height: 75px;
-  font-size: 30px;
-  color: white;
-  position: absolute;
-  top: 200px;
-  left: 420px;
-  background-color: rgba(128, 128, 128, 0.5);
-`
